ce8084009edd2eb4d5709f4ab324437afedd7c33
alanhidayat33 <alanarbanihidayat92@gmail.com>
2023-03-22 12:29:50 +0700

    percobaan 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_state_management/main_screen_percobaan1.dart';
+import 'package:flutter_state_management/main_screen_percobaan2.dart';
+import 'package:flutter_state_management/provider/done_tourism_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +13,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'State Management',
-      theme: ThemeData(),
-
-      home: MainScreen(),
+    return ChangeNotifierProvider(
+      create: (context) => DoneTourismProvider(),
+      child: MaterialApp(
+        title: 'Provider',
+        theme: ThemeData(),
+        home: MainScreen(),
+      ),
     );
   }
 }
